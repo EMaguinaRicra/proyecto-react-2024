00b7999a35e6689a8ddc7c13bd742be54e37cd37
Eloy Maguiña Ricra <138701348+EMaguinaRicra@users.noreply.github.com>
2024-06-29 16:56:56 -0500

    Se importó la librería Dayjs para hacer la actividad del historial de criptomonedas, se utilizaron dos useState para la resolución de la actividad

--- src/Components/Crypto/PageCrypto.jsx
+++ src/Components/Crypto/PageCrypto.jsx
@@ -1,12 +1,70 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import dayjs from "dayjs";
 
 const PageCrypto = () => {
   const params = useParams();
 
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const [crypto, setCrypto] = useState({});
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}assets/${params.id}`)
+      .then((data) => {
+        setCrypto(data.data.data);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}assets/${params.id}/history?interval=d1`)
+      .then((data) => {
+        setHistory(data.data.data);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
   return (
     <>
-      <h1>PageCrypto</h1>
-      <p className="info">Im the Crypto {params.id} </p>
+      <h1>Page of Crypto: {crypto.name}</h1>
+
+      <div className="table">
+        <ul>
+          <li>
+            <span className="info label">Nombre: </span>{" "}
+            <span className="info">{crypto.name}</span>
+          </li>
+          <li>
+            <span className="info label">Sìmbolo: </span>{" "}
+            <span className="info">{crypto.symbol}</span>
+          </li>
+        </ul>
+      </div>
+
+      <h2 className="info">Hisotiral</h2>
+      <p className="info">Historial de la criptomoneda</p>
+
+      <table className="info">
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {history.map(({ date, priceUsd, time }) => (
+            <tr key={time}>
+              <td>{dayjs(date).format("DD/MM/YYYY")}</td>
+              <td>{parseFloat(priceUsd).toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
